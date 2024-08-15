61a2514ae49ae0fd7d9c45e69bf549abd6412af8
akai <2780896955@qq.com>
2024-08-15 05:51:04 +0000

    process reset return None

--- voyager/env/bridge.py
+++ voyager/env/bridge.py
@@ -246,6 +246,9 @@ class VoyagerEnv(gym.Env):
         # All the reset in step will be soft
         self.reset_options["reset"] = "soft"
         self.pause()
+        if returned_data is None:
+            self.logger.warning('reset return None')
+            return None        
         return json.loads(returned_data)
 
     def close(self):
